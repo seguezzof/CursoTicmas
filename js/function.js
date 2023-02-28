aa3163f9472f46da875dc895820528b17b7c50bf
Franco Seguezzo <seguezzo27@gmail.com>
2023-03-01 00:00:10 +0100

    Primer escritura en el repositorio

---
+++ js/function.js
@@ -0,0 +1,7 @@
+document.getElementById('boton_fondo_noc').addEventListener('click',function(){
+    document.body.style.backgroundColor='#1f4060';
+});
+
+document.getElementById('boton_fondo_diu').addEventListener('click',function(){
+    document.body.style.backgroundColor='#ffffff';
+});
